ec824e71a954d8700d4da28437b49d9339794933
Jonathon Bloomfield <bloomfield.codes@gmail.com>
2021-10-11 18:49:55 +0000

    Done exercise 6!

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Khurrem", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Khurrim", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Khurrom", last_name: "Virani", hourly_rate: 60)
+@store3.employees.create(first_name: "Khurrum", last_name: "Virani", hourly_rate: 60)
+@store4.employees.create(first_name: "Khurrym", last_name: "Virani", hourly_rate: 60)
